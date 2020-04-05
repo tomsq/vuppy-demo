3b33707788de0d0e3555995d7a712eb008ac1281
Tomas <finixit@email.cz>
2020-04-05 22:52:32 +0200

    split pen added

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,81 @@
 <template>
-  <div class="h-screen">
-    <h1 class="pt-10 flex justify-center text-4xl text-gray-800">Vuppy - Demo</h1>
+  <div>
+    <div class="h-screen hidden sm:block">
+      <split-pane  @resize="resize" :min-percent="20" :default-percent="50" split="vertical">
+        <template slot="paneL">
+          <div class="flex justify-start pl-8 pt-10">
+            <toggle-button :value="mobileToggle" @input="mobileToggled"></toggle-button>
+          </div>
+          <div class="pt-48 px-8">
+            <vuppy v-model="images"></vuppy>
+          </div>
+        </template>
+        <template slot="paneR"> 
+          <div class="bg-gray-200 h-screen">
+            <h1 class="pt-8 flex justify-center text-4xl text-gray-700">Vuppy - Demo</h1>
+            <div class="flex justify-center pt-8 shadow-md pb-6">
+              <a @click="images = []" class="bg-teal-700 text-white px-4 py-2 rounded cursor-pointer hover:bg-teal-600">Clear Images Externally</a>
+            </div>
+            <div class="overflow-y-auto overflow-x-auto  rounded" style="height: 46rem;">
+              <json-view class="pt-4 pl-4" :data="strippedData" :max-depth="3" rootKey="images"/>
+            </div>
+          </div>
+        </template>    
+      </split-pane>
+    </div>
+    <div class="h-screen sm:hidden">
+      <h1 class="pt-8 flex justify-center text-4xl text-gray-200">Vuppy - Demo</h1>
 
-    <div class="md:flex md:w-full md:justify-around items-center pt-16 px-8">
-      <div class="w-full md:w-1/2 flex justify-center">
-        <vuppy class="pb-16" v-model="images" :dashboardHeight="400" :dashboardWidth="600"></vuppy>
+      <div class="pt-12 px-8">
+        <vuppy v-model="images"></vuppy>
       </div>
 
-      <div class="w-full md:w-1/2">
-
-        <a @click="images = []" class="bg-teal-700 text-white px-4 py-2 rounded cursor-pointer hover:bg-teal-600">Clear Images Externally</a>
-
-        <div class="mt-10 overflow-y-auto overflow-x-auto bg-gray-400 rounded" style="height: 33rem;">
+      <div class="bg-gray-200 mt-20">
+        <div class="flex justify-center pt-8 shadow-md pb-6">
+          <a @click="images = []" class="bg-teal-700 text-white px-4 py-2 rounded cursor-pointer hover:bg-teal-600">Clear Images Externally</a>
+        </div>
+        <div class="overflow-y-auto overflow-x-auto  rounded" style="height: 46rem;">
           <json-view class="pt-4 pl-4" :data="strippedData" :max-depth="3" rootKey="images"/>
         </div>
       </div>
     </div>
   </div>
 </template>
-
 <script>
 
-import vuppy from 'vuppy'
+import vuppy from 'vuppy';
 import { JSONView } from 'vue-json-component';
-
+import splitPane from 'vue-splitpane';
+import ToggleButton from './components/ToggleButton';
 
 export default {
   components: {
     vuppy,
-    'json-view': JSONView
+    'json-view': JSONView,
+    splitPane,
+    ToggleButton
   },
 
   data() {
     return {
-      images: []
+      images: [],
+      mobileToggle: false
+    }
+  },
+
+  methods: {
+    resize() {
+      if (this.$children[0].percent > 50) 
+        { this.$children[0].percent = 50 }
+    },
+    mobileToggled() {
+      this.mobileToggle = !this.mobileToggle;
+
+      if(this.mobileToggle === true) {
+        this.$children[0].percent = 20;
+      } else {
+        this.$children[0].percent = 50;
+      }
     }
   },
 
@@ -42,15 +84,13 @@ export default {
       let strippedData = []
       this.images.forEach(image => {
         strippedData.push({
-          file: image.file.substring(0,32) + '...',
+          file: image.file.substring(0,50) + '...',
           id: image.id,
           name: image.name
         })
       });
-
       return strippedData;
     }
   }
-
 }
 </script>
---
+++ src/components/ToggleButton.vue
@@ -0,0 +1,68 @@
+<template>
+<div>
+  <!-- Toggle Button -->
+  <label 
+    for="toogleA"
+    class="flex items-center cursor-pointer"
+  >
+    <!-- toggle -->
+    <div class="relative">
+      <!-- input -->
+      <input id="toogleA" type="checkbox" class="hidden" :value="value" @input="valueChanged" />
+      <!-- line -->
+      <div
+        class="toggle__line w-10 h-4 bg-gray-400 rounded-full shadow-inner"
+      ></div>
+      <!-- dot -->
+      <div
+        class="toggle__dot absolute w-6 h-6 bg-white rounded-full shadow inset-y-0 left-0"
+      ></div>
+    </div>
+    <!-- label -->
+    <div
+      class="ml-3 text-gray-200 font-medium"
+    >
+       {{ buttonText }}
+    </div>
+  </label>
+  
+</div>
+</template>
+
+<script>
+export default {
+    props: {
+        value: {
+            type: Boolean,
+            default: false
+        }
+    },
+    methods: {
+        valueChanged(e) {
+            this.$emit('input', e.target.checked);
+        }
+    },
+
+    computed: {
+        buttonText() {
+            if(this.value) {
+                return 'Toggle Mobile';
+            } else {
+                return 'Toggle Desktop';
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+.toggle__dot {
+  top: -.25rem;
+  left: -.25rem;
+  transition: all 0.3s ease-in-out;
+}
+
+input:checked ~ .toggle__dot {
+  transform: translateX(100%);
+}
+</style>
