6a044d766e71fccb00367dba6dbef3c3832a7af4
Tomas <finixit@email.cz>
2020-04-05 17:16:17 +0200

    json view added

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,56 @@
 <template>
-  <div>
-    <vuppy v-model="images" locale="cs_CZ"></vuppy>
+  <div class="h-screen">
+    <h1 class="pt-10 flex justify-center text-4xl">Vuppy - Demo</h1>
+
+    <div class="md:flex md:w-full md:justify-around items-center pt-16 px-8">
+      <div class="w-full md:w-1/2 flex justify-center">
+        <vuppy class="pb-16" v-model="images" :dashboardHeight="400" :dashboardWidth="600"></vuppy>
+      </div>
+
+      <div class="w-full md:w-1/2">
+
+        <a @click="images = []" class="bg-teal-700 text-white px-4 py-2 rounded cursor-pointer hover:bg-teal-600">Clear Images Externally</a>
+
+        <div class="mt-10 overflow-y-auto overflow-x-auto bg-gray-400 rounded" style="height: 33rem;">
+          <json-view class="pt-4 pl-4" :data="strippedData" :max-depth="3" rootKey="images"/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 
 import vuppy from 'vuppy'
+import { JSONView } from 'vue-json-component';
+
 
 export default {
   components: {
-    vuppy
+    vuppy,
+    'json-view': JSONView
   },
 
   data() {
     return {
       images: []
     }
+  },
+
+  computed: {
+    strippedData() {
+      let strippedData = []
+      this.images.forEach(image => {
+        strippedData.push({
+          file: image.file.substring(0,32) + '...',
+          id: image.id,
+          name: image.name
+        })
+      });
+
+      return strippedData;
+    }
   }
 
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
